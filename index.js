186c8f67e54bb36d65c950d1d5462b296f6c7da5
SteveSandersonMS <stevesa@microsoft.com>
2017-01-31 14:22:50 +0000

    Initial version

---
+++ index.js
@@ -0,0 +1,33 @@
+/*
+ * THIS IS TEMPORARY TO PATCH 2.1.1+ Core bugs
+ */
+var semver = require('semver');
+var __core__ = require('@angular/core');
+var coreVersion = __core__ && __core__.VERSION && __core__.VERSION.full;
+
+// Only patch if you're on Angular >= 2.1.1 and < the next major version (including prerelease)
+if (coreVersion && semver.satisfies(coreVersion, '^2.1.1')) {
+    var __compiler__ = require('@angular/compiler');
+    var __core_private__ = __core__.__core_private__;
+
+    var patch = false;
+    if (!__core_private__['ViewUtils']) {
+        patch = true;
+        __core_private__['ViewUtils'] = __core_private__['view_utils'];
+    }
+
+    if (!__compiler__.__compiler_private__) {
+        patch = true;
+        __compiler__.__compiler_private__ = {
+            SelectorMatcher: __compiler__.SelectorMatcher,
+            CssSelector: __compiler__.CssSelector
+        }
+    }
+
+    var __universal__ = require('angular2-platform-node/__private_imports__');
+    if (patch) {
+        __universal__.ViewUtils = __core_private__['view_utils'];
+        __universal__.CssSelector = __compiler__.CssSelector
+        __universal__.SelectorMatcher = __compiler__.SelectorMatcher
+    }
+}
